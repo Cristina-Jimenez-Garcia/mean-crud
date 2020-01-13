ff7e23e6fd7361dc694524834b6e460c177642c8
Cristina-Jimenez-Garcia <crstnjmnzgrc@gmail.com>
2020-01-13 19:49:15 +0100

    update commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
+const { mongoose }= require('./database');
 
-app.listen(3000,()=>{
+//Settings (configuraciones básicas el servidor)
+app.set('port', process.env.PORT || 3000);
+
+//Middlewares (librerias)
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/teachers', require('./routers/teachers.routers'));
+
+//Starting the server
+app.listen(app.get('port'),()=>{
     console.log('Server on port 3000');
 });
